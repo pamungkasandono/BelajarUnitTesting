f966bac27e352e3033995ce3d090aef96d800d2b
pamungkas andono <pamungkas.org@gmail.com>
2021-03-23 16:35:24 +0700

    Penggantian findViewById dengan viewBinding

--- app/src/main/java/com/example/myunittesting/MainActivity.kt
+++ app/src/main/java/com/example/myunittesting/MainActivity.kt
@@ -6,53 +6,59 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myunittesting.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var _mainViewModel: MainViewModel
 
-    private lateinit var _etPanjang: EditText
-    private lateinit var _etLebar: EditText
-    private lateinit var _etTinggi: EditText
-    private lateinit var _btnHitung: Button
-    private lateinit var _tvHasil: TextView
-    private lateinit var _btnHitVolume: Button
-    private lateinit var _btnHitLuasPermukaan: Button
-    private lateinit var _btnHitKeliling: Button
+    private lateinit var binding: ActivityMainBinding
+
+//    private lateinit var _etPanjang: EditText
+//    private lateinit var _etLebar: EditText
+//    private lateinit var _etTinggi: EditText
+//    private lateinit var _btnHitung: Button
+//    private lateinit var _tvHasil: TextView
+//    private lateinit var _btnHitVolume: Button
+//    private lateinit var _btnHitLuasPermukaan: Button
+//    private lateinit var _btnHitKeliling: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         _mainViewModel = MainViewModel(CuboidModel())
 
-        _etLebar = findViewById(R.id.et_lebar)
-        _etPanjang = findViewById(R.id.et_panjang)
-        _etTinggi = findViewById(R.id.et_tinggi)
-        _tvHasil = findViewById(R.id.tv_hasil)
-        _btnHitung = findViewById(R.id.btn_hitung)
-        _btnHitVolume = findViewById(R.id.btn_hit_volume)
-        _btnHitLuasPermukaan = findViewById(R.id.btn_hit_luas_permukaan)
-        _btnHitKeliling = findViewById(R.id.btn_hit_keliling)
+//        _etLebar = findViewById(R.id.et_lebar)
+//        _etPanjang = findViewById(R.id.et_panjang)
+//        _etTinggi = findViewById(R.id.et_tinggi)
+//        _tvHasil = findViewById(R.id.tv_hasil)
+//        _btnHitung = findViewById(R.id.btn_hitung)
+//        _btnHitVolume = findViewById(R.id.btn_hit_volume)
+//        _btnHitLuasPermukaan = findViewById(R.id.btn_hit_luas_permukaan)
+//        _btnHitKeliling = findViewById(R.id.btn_hit_keliling)
 
-        _btnHitung.setOnClickListener(this)
-        _btnHitVolume.setOnClickListener(this)
-        _btnHitLuasPermukaan.setOnClickListener(this)
-        _btnHitKeliling.setOnClickListener(this)
+
+
+        binding.btnHitung.setOnClickListener(this)
+        binding.btnHitVolume.setOnClickListener(this)
+        binding.btnHitLuasPermukaan.setOnClickListener(this)
+        binding.btnHitKeliling.setOnClickListener(this)
 
 
     }
 
     override fun onClick(v: View?) {
-        val lebar = _etLebar.text.toString().trim()
-        val panjang = _etPanjang.text.toString().trim()
-        val tinggi = _etTinggi.text.toString().trim()
+        val lebar = binding.etLebar.text.toString().trim()
+        val panjang = binding.etPanjang.text.toString().trim()
+        val tinggi = binding.etTinggi.text.toString().trim()
 
         val errorText = "Field ini tidak boleh kosong"
         when {
-            lebar.isEmpty() -> _etLebar.error = errorText
-            panjang.isEmpty() -> _etPanjang.error = errorText
-            tinggi.isEmpty() -> _etTinggi.error = errorText
+            lebar.isEmpty() -> binding.etLebar.error = errorText
+            panjang.isEmpty() -> binding.etPanjang.error = errorText
+            tinggi.isEmpty() -> binding.etTinggi.error = errorText
             else -> {
                 val l = lebar.toDouble()
                 val p = panjang.toDouble()
@@ -64,15 +70,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                         tampil()
                     }
                     R.id.btn_hit_volume -> {
-                        _tvHasil.text = _mainViewModel.getVolume().toString()
+                        binding.tvHasil.text = _mainViewModel.getVolume().toString()
                         sembunyi()
                     }
                     R.id.btn_hit_luas_permukaan -> {
-                        _tvHasil.text = _mainViewModel.getLuasPermukaan().toString()
+                        binding.tvHasil.text = _mainViewModel.getLuasPermukaan().toString()
                         sembunyi()
                     }
                     R.id.btn_hit_keliling -> {
-                        _tvHasil.text = _mainViewModel.getKeliling().toString()
+                        binding.tvHasil.text = _mainViewModel.getKeliling().toString()
                         sembunyi()
                     }
                 }
@@ -80,15 +86,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
     private fun tampil() {
-        _btnHitVolume.visibility = View.VISIBLE
-        _btnHitLuasPermukaan.visibility = View.VISIBLE
-        _btnHitKeliling.visibility = View.VISIBLE
-        _btnHitung.visibility = View.GONE
+        binding.btnHitVolume.visibility = View.VISIBLE
+        binding.btnHitLuasPermukaan.visibility = View.VISIBLE
+        binding.btnHitKeliling.visibility = View.VISIBLE
+        binding.btnHitung.visibility = View.GONE
     }
     private fun sembunyi() {
-        _btnHitVolume.visibility = View.GONE
-        _btnHitLuasPermukaan.visibility = View.GONE
-        _btnHitKeliling.visibility = View.GONE
-        _btnHitung.visibility = View.VISIBLE
+        binding.btnHitVolume.visibility = View.GONE
+        binding.btnHitLuasPermukaan.visibility = View.GONE
+        binding.btnHitKeliling.visibility = View.GONE
+        binding.btnHitung.visibility = View.VISIBLE
     }
 }
